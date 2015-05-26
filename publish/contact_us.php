4deb06ada6e88f87668e04cfc0dddac7db8a6d6c
Laurie Fumarolo <pwdlaurie@gmail.com>
2015-05-26 17:46:50 -0500

    Adding a Google map to Contact Us page.

--- publish/contact_us.php
+++ publish/contact_us.php
@@ -28,26 +28,36 @@
 				<?php include 'sidebar.php' ?>
 			</div>
 			<div class="col-sm-8 content-col">
-				<h2>Contact Us</h2>
-				<form action="" method="post" class="form-horizontal" role="form" data-toggle="validator">
-					<div class="form-group">
-						<label for="inputEmail" class="col-sm-2 control-label">Email</label>
-						<div class="col-sm-10">
-							<input type="email" id="inputEmail" class="form-control" placeholder="Email" data-error="This email address is invalid." required>
-							<div class="help-block with-errors"></div>
+				<div class="clearfix">
+					<h2>Contact Us</h2>
+					<form action="" method="post" class="form-horizontal" role="form" data-toggle="validator">
+						<div class="form-group">
+							<label for="inputEmail" class="col-sm-2 control-label">Email</label>
+							<div class="col-sm-10">
+								<input type="email" id="inputEmail" class="form-control" placeholder="Email" data-error="This email address is invalid." required>
+								<div class="help-block with-errors"></div>
+							</div>
 						</div>
-					</div>
-					<div class="form-group">
-						<label for="comments" class="col-sm-2 control-label">Comments</label>
-						<div class="col-sm-10">
-							<textarea id="comments" class="form-control" rows="5" placeholder="Comments" required></textarea>
-							<div class="help-block with-errors"></div>
+						<div class="form-group">
+							<label for="comments" class="col-sm-2 control-label">Comments</label>
+							<div class="col-sm-10">
+								<textarea id="comments" class="form-control" rows="5" placeholder="Comments" required></textarea>
+								<div class="help-block with-errors"></div>
+							</div>
 						</div>
+						<div class="form-group col-sm-12 control-label">
+							<button type="submit" class="btn btn-primary">Submit</button>
+						</div>
+					</form>
+				</div>
+				<div class="clearfix">
+					<hr />
+					<h2>Come Visit Us</h2>
+					<div class="col-sm-12 embed-responsive embed-responsive-16by9">
+						<!-- Google map of planetarium location -->
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2971.230801056126!2d-87.6068281!3d41.866380500000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880e2c9d42b46935%3A0x4fe61de107e3c4ab!2s1300+S+Lake+Shore+Dr%2C+Chicago%2C+IL+60605!5e0!3m2!1sen!2sus!4v1432679251654" frameborder="0"></iframe>
 					</div>
-					<div class="form-group col-sm-12 control-label">
-						<button type="submit" class="btn btn-primary">Submit</button>
-					</div>
-				</form>
+				</div>
 			</div>
 		</div>
 	</div>

6dfc1530f1693b824eb441919e185e392645ac46
Laurie Fumarolo <pwdlaurie@gmail.com>
2015-05-29 15:23:46 -0500

    Changing layout of Tickets page.

--- publish/tickets.php
+++ publish/tickets.php
@@ -24,25 +24,32 @@
 		<?php include 'header.php' ?>
 	</header>
 	<div class="container-fluid content-container">
-		<h2>Admission &amp; Passes</h2>
-
-		<p class="bg-primary bg-gold text-center text-uppercase">All Access Pass</p>
-		<p>Enjoy VIP access to all of our exhibits and unlimited shows. This pass includes 6 months membership to the Stargazer's Club.</p>
-		<p>VIPs will also be given priority seating.</p>
-		<p class="price">$75 Adult/$55 Child</p>
-		<button type="button" class="btn btn-primary btn-gold">Purchase</button>
-
-		<p class="bg-primary bg-silver text-center text-uppercase top-space">Limited Pass</p>
-		<p>This is truly the best value! Get unlimited access to all of our exhibits and admission to two shows.</p>
-		<p>The Limited Pass also includes one month membership to the Stargazer's Club.</p>
-		<p class="price">$50 Adult/$30 Child</p>
-		<button type="button" class="btn btn-primary btn-silver">Purchase</button>
-
-		<p class="bg-primary bg-bronze text-center text-uppercase top-space">Basic Pass</p>
-		<p>Get unlimited access to all of our exhibits.</p>
-		<p>The Basic Pass also includes admission to one show.</p>
-		<p class="price">$25 Adult/$15 Child</p>
-		<button type="button" class="btn btn-primary btn-bronze">Purchase</button>
+		<div class="clearfix">
+			<h2>Admission &amp; Passes</h2>
+		</div>
+		<div class="row">
+			<div class="col-sm-4 ticket-col">
+				<p class="bg-primary bg-gold text-center text-uppercase">All Access Pass</p>
+				<p>Enjoy VIP access to all of our exhibits and unlimited shows. This pass includes 6 months membership to the Stargazer's Club.</p>
+				<p>VIPs will also be given priority seating.</p>
+				<p class="price">$75 Adult/$55 Child</p>
+				<button type="button" class="btn btn-primary btn-gold">Purchase</button>
+			</div>
+			<div class="col-sm-4 ticket-col">
+				<p class="bg-primary bg-silver text-center text-uppercase">Limited Pass</p>
+				<p>This is truly the best value! Get unlimited access to all of our exhibits and admission to two shows.</p>
+				<p>The Limited Pass also includes one month membership to the Stargazer's Club.</p>
+				<p class="price">$50 Adult/$30 Child</p>
+				<button type="button" class="btn btn-primary btn-silver">Purchase</button>
+			</div>
+			<div class="col-sm-4 ticket-col">
+				<p class="bg-primary bg-bronze text-center text-uppercase">Basic Pass</p>
+				<p>Get unlimited access to all of our exhibits.</p>
+				<p>The Basic Pass also includes admission to one show.</p>
+				<p class="price">$25 Adult/$15 Child</p>
+				<button type="button" class="btn btn-primary btn-bronze">Purchase</button>
+			</div>
+		</div>
 	</div>
 	<footer>
 		<?php include 'footer.php' ?>

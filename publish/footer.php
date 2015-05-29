c5846035258927f8a243bbf2e00fd0e60f331787
Laurie Fumarolo <pwdlaurie@gmail.com>
2015-05-29 15:05:26 -0500

    Fixing an issue where the hamburger menu button was transparent. Adding more content to the footer (moving from the main navigation).

--- publish/footer.php
+++ publish/footer.php
@@ -1,5 +1,25 @@
 <div class="container-fluid text-center">
-	<p>Hours: 9:00 am - 5:00 pm MON-FRI, 10:00 am - 8:00 pm SAT-SUN</p>
-	<p class="small">Copyright &copy;2015 Laurie Fumarolo. All Rights Reserved.</p>
-	<h5 class="text-danger">NOT A REAL BUSINESS. FOR EDUCATIONAL PURPOSES ONLY.</h5>
+	<div class="row upper-footer">
+		<div class="col-sm-4">
+			<p class="small">Hours:</p>
+			<p class="small">9:00am - 5:00pm MON-FRI</p>
+			<p class="small">10:00am - 8:00pm SAT-SUN</p>
+		</div>
+		<div class="col-sm-4">
+			<p class="small">Address:</p>
+			<p class="small">1300 S Lake Shore Drive</p>
+			<p class="small">Chicago, <abbr title="Illinois">IL</abbr> 60605</p>
+		</div>
+		<div class="col-sm-4">
+			<p class="small"><a href="about_us.php">About Us</a></p>
+			<p class="small"><a href="contact_us.php">Contact Us</a></p>
+			<p class="small"><a href="#">Acknowledgments</a></p>
+		</div>
+	</div>
+	<div class="row lower-footer">
+		<div class="col-sm-12">
+			<p class="small">Copyright &copy;2015 Laurie Fumarolo. All Rights Reserved.</p>
+			<h5 class="text-danger">NOT A REAL BUSINESS. FOR EDUCATIONAL PURPOSES ONLY.</h5>
+		</div>
+	</div>
 </div>

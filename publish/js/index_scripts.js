b72b227cb5b4a93c887a5fee0f8e6a748ea18e48
Laurie Fumarolo <pwdlaurie@gmail.com>
2015-05-31 21:02:46 -0500

    Fixing an issue where the banner image was not 100% width in IE and Mozilla. Making the navbar black on all mobile-sized screens.

--- publish/js/index_scripts.js
+++ publish/js/index_scripts.js
@@ -1,17 +1,29 @@
 $(document).ready(function() {
-	// on the home page, set the navbar to be transparent initially
-	$("nav").addClass("navbar-index");
+	// only run on desktop
+	if ($(window).width() > 753) {
+		// on the home page, set the navbar to be transparent initially
+		$("nav").addClass("navbar-index");
 
-	// make the navbar transparent unless the user has scrolled down to the top of the banner quote
-	// if the user scrolls back up from that point, make the navbar transparent again
-	$(window).scroll(function() {
-		// add animation class, if not already added
-		// this will make the transition from transparent nav to black nav smoother
-		if (!$("nav").hasClass("navbar-transition")) {
-			$("nav").addClass("navbar-transition");
-		}
+		// make the navbar transparent unless the user has scrolled down to the top of the banner quote
+		// if the user scrolls back up from that point, make the navbar transparent again
+		$(window).scroll(function() {
+			// add animation class, if not already added
+			// this will make the transition from transparent nav to black nav smoother
+			if (!$("nav").hasClass("navbar-transition")) {
+				$("nav").addClass("navbar-transition");
+			}
 
-		if ($(this).scrollTop() > 180) {
+			if ($(this).scrollTop() > 180) {
+				$("nav").removeClass("navbar-index");
+			} else {
+				$("nav").addClass("navbar-index");
+			}
+		});
+	}
+
+	// for the desktop, when the user resizes the screen to mobile-size
+	$(window).resize(function() {
+		if ($(window).width() <= 752) {
 			$("nav").removeClass("navbar-index");
 		} else {
 			$("nav").addClass("navbar-index");

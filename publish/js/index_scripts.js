7ddf2913c947693aea65839adf2dfd23fc6326cf
Laurie Fumarolo <pwdlaurie@gmail.com>
2015-05-31 21:44:58 -0500

    Fixing an issue with the navbar being transparent on window resize when the user is scrolled below the banner quote.

--- publish/js/index_scripts.js
+++ publish/js/index_scripts.js
@@ -1,32 +1,35 @@
 $(document).ready(function() {
-	// only run on desktop
-	if ($(window).width() > 753) {
-		// on the home page, set the navbar to be transparent initially
-		$("nav").addClass("navbar-index");
-
-		// make the navbar transparent unless the user has scrolled down to the top of the banner quote
-		// if the user scrolls back up from that point, make the navbar transparent again
-		$(window).scroll(function() {
-			// add animation class, if not already added
-			// this will make the transition from transparent nav to black nav smoother
-			if (!$("nav").hasClass("navbar-transition")) {
-				$("nav").addClass("navbar-transition");
-			}
-
-			if ($(this).scrollTop() > 180) {
-				$("nav").removeClass("navbar-index");
-			} else {
-				$("nav").addClass("navbar-index");
-			}
-		});
+	// if the window is not mobile-size and the user is scrolled above the banner quote, show transparent navbar
+	function addRemoveTransparent() {
+		if ($(window).width() > 753 && $(this).scrollTop() <= 180) {
+			$("nav").addClass("navbar-index");
+		} else {
+			$("nav").removeClass("navbar-index");
+		}
 	}
+
+	addRemoveTransparent();
+
+	// make the navbar transparent unless the user has scrolled down to the top of the banner quote
+	// if the user scrolls back up from that point, make the navbar transparent again
+	$(window).scroll(function() {
+		// add animation class, if not already added
+		// this will make the transition from transparent nav to black nav smoother
+		if (!$("nav").hasClass("navbar-transition")) {
+			$("nav").addClass("navbar-transition");
+		}
+
+		addRemoveTransparent();
+	});
 
 	// for the desktop, when the user resizes the screen to mobile-size
 	$(window).resize(function() {
-		if ($(window).width() <= 752) {
-			$("nav").removeClass("navbar-index");
-		} else {
-			$("nav").addClass("navbar-index");
+		// add animation class, if not already added
+		// this will make the transition from transparent nav to black nav smoother
+		if (!$("nav").hasClass("navbar-transition")) {
+			$("nav").addClass("navbar-transition");
 		}
+
+		addRemoveTransparent();
 	});
 });
